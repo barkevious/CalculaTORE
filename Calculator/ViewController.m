891e19601bc74e39035d79ab751c2bca62374f87
Ryan Verner <ryan@8thlight.com>
2014-02-10 22:42:34 -0600

    Add simple calculator app.

---
+++ Calculator/ViewController.m
@@ -0,0 +1,30 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)modifyDisplayByAppendingString:(NSString *)string
+{
+    self.display.text = [self.display.text stringByAppendingString:[NSString stringWithFormat:@" %@ ", string]];
+}
+
+- (IBAction)buttonPressed:(id)sender
+{
+    [self modifyDisplayByAppendingString:[(UIButton *)sender currentTitle]];
+}
+
+- (IBAction)evaluateDisplay:(id)sender
+{
+    NSExpression *expression = [NSExpression expressionWithFormat:self.display.text];
+    self.display.text = [NSString stringWithFormat:@"%@", [expression expressionValueWithObject:nil context:nil]];
+}
+
+- (IBAction)clearDisplay:(id)sender
+{
+    self.display.text = @"";
+}
+
+@end
